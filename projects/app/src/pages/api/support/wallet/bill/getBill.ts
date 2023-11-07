f28fc8c0f0213cc0fe6e948f961795562c0f0481
archer <545436317@qq.com>
2023-11-07 19:45:05 +0800

    fix: ts

--- projects/app/src/pages/api/support/wallet/bill/getBill.ts
+++ projects/app/src/pages/api/support/wallet/bill/getBill.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { jsonRes } from '@fastgpt/service/common/response';
 import { connectToDatabase } from '@/service/mongo';
 import { MongoBill } from '@fastgpt/service/support/wallet/bill/schema';
-import { adaptBill, formatPrice } from '@fastgpt/global/support/wallet/bill/tools';
+import { formatPrice } from '@fastgpt/global/support/wallet/bill/tools';
 import { addDays } from 'date-fns';
 import { authUserRole } from '@fastgpt/service/support/permission/auth/user';
 import { Types } from '@fastgpt/service/common/mongo';
