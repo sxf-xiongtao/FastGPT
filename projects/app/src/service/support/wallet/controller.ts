375708b74c2f8d1199af519ab6ddea4de3226ee9
archer <545436317@qq.com>
2023-11-14 09:28:22 +0800

    perf: log

--- projects/app/src/service/support/wallet/controller.ts
+++ projects/app/src/service/support/wallet/controller.ts
@@ -5,7 +5,7 @@ import { addLog } from '@fastgpt/service/common/mongo/controller';
     amount: min unit
 */
 export async function updateTeamBalance({ teamId, amount }: { teamId: string; amount: number }) {
-  if (Math.abs(amount) < 50) {
+  if (Math.abs(amount) < 10) {
     addLog.info('updateTeamBalance amount too small, maybe have error', { teamId, amount });
   }
 
