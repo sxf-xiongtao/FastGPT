d6ba58d5c6079d91bdcb7aa6c3c829c2036a3597
archer <545436317@qq.com>
2023-11-15 10:42:47 +0800

    perf: invite

--- projects/app/src/pages/api/support/user/team/member/invite.ts
+++ projects/app/src/pages/api/support/user/team/member/invite.ts
@@ -58,6 +58,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       });
       if (leaveTmb) {
         leaveTmb.status = TeamMemberStatusEnum.waiting;
+        leaveTmb.role = role;
         await leaveTmb.save();
         continue;
       }
