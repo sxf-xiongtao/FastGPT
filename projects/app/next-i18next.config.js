30288acdd88f32ef417e6216b40134a563d72e5e
heheer <1239331448@qq.com>
2023-12-08 17:14:02 +0800

    i18n

--- projects/app/next-i18next.config.js
+++ projects/app/next-i18next.config.js
@@ -6,7 +6,10 @@
 module.exports = {
   i18n: {
     defaultLocale: 'zh',
-    locales: ['en', 'zh'],
+    locales: ['en', 'zh', 'zh-Hans', 'zh-CN'],
     localeDetection: false
-  }
+  },
+  localePath:
+    typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
+  reloadOnPrerender: process.env.NODE_ENV === 'development'
 };
