b90b76e425fd2cbfb9b747cbf4509e366e7d3335
archer <545436317@qq.com>
2024-07-17 15:25:01 +0800

    perf: init system

--- projects/app/src/service/mongo.ts
+++ projects/app/src/service/mongo.ts
@@ -5,16 +5,22 @@ import { connectMongo } from '@fastgpt/service/common/mongo/init';
 import { concatBillTimer, reduceAiPointsTimer } from './support/wallet/controller';
 import { startCron } from './common/system/cron';
 import { startTrainingProcess } from './core/dataset/training/utils';
+import { addLog } from '@fastgpt/service/common/system/log';
 
 /**
  * connect MongoDB and init data
  */
 export async function connectToDatabase(): Promise<void> {
-  await connectMongo({
-    beforeHook: () => {
-      initGlobal();
-    },
-    afterHook: async () => {
+  if (!global.systemLoadedGlobalVariables) {
+    global.systemLoadedGlobalVariables = true;
+    initGlobal();
+  }
+
+  await connectMongo().then(async () => {
+    if (global.systemLoadedGlobalConfig) return;
+    global.systemLoadedGlobalConfig = true;
+
+    try {
       startCron();
       reduceAiPointsTimer();
       concatBillTimer();
@@ -24,6 +30,9 @@ export async function connectToDatabase(): Promise<void> {
       await Promise.all([getProInitData(), authLicense()]);
 
       startTrainingProcess();
+    } catch (error) {
+      addLog.error('init error', error);
+      exit(1);
     }
   });
 }
