0d1916bb4faf2febe2a43580990c137b56b9604c
Finley Ge <32237950+FinleyGe@users.noreply.github.com>
2024-10-25 10:48:52 +0800

    pref(plugin): dynamic web sync excludsion of domains controlled via env (#203)
    * pref: add environment of exclude domains
    
    * feat: control the static web sync exclude list with a env var

--- projects/plugin/src/index.ts
+++ projects/plugin/src/index.ts
@@ -9,6 +9,8 @@ app.get('/api/dynamicWebsiteCrawler/query', query);
 app.get('/api/dynamicWebsiteCrawler/stop', stop);
 
 app.listen(PORT, () => {
+  console.log('Max Concurrency: ', process.env.CRAWL_MAX_CONCURRENCY);
+  console.log('Exclude Domains: ', process.env.CRAWL_EXCLUDE_LIST);
   console.log(`Server is running on http://localhost:${PORT}`);
   console.log('Start a dynamic website crawler: /api/dynamicWebsiteCrawler/start');
   console.log('Query a dynamic website crawler: /api/dynamicWebsiteCrawler/query');
