224334ed33915a4e24ac53fd16d08045fef5da97
Archer <545436317@qq.com>
2025-02-28 12:40:02 +0800

    update prompt (#371)

--- projects/app/src/global/core/ai/prompt/autoTraining.ts
+++ projects/app/src/global/core/ai/prompt/autoTraining.ts
@@ -2,7 +2,7 @@ import { replaceVariable } from '@fastgpt/global/common/string/tools';
 
 export const getAutoTrainingPrompt = ({ text }: { text: string }) => {
   const AUTO_TRAINING_PROMPT = `你是一个阅读理解专家。我会发给你一段文本，请学习它并完成任务：
-1. 为它生成一些可能的问题，至少 3 个。
+1. 为它生成一些可能的问题，至少 5 个。
 2. 为它们生成总结。
 3. 要求输出的语言与源文本语言相同，输出的总结要完整全面。
 
