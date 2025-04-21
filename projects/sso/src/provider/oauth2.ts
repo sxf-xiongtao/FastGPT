f6de15a3f4dd98ceb83e25910effb02f128af7a5
Finley Ge <32237950+FinleyGe@users.noreply.github.com>
2025-04-21 10:54:19 +0800

    perf: oauth2 sso support customize request method (#519)

--- projects/sso/src/provider/oauth2.ts
+++ projects/sso/src/provider/oauth2.ts
@@ -1,9 +1,18 @@
 import axios from 'axios';
 import type { GetUserInfoFn, RedirectFn } from 'type';
 
+type RequestMethod = 'get' | 'post';
+type TokenMethod = 'header' | RequestMethod;
+
 const OAuth2AuthorizeURL = process.env.OAUTH2_AUTHORIZE_URL || '';
 const OAuth2TokenURL = process.env.OAUTH2_TOKEN_URL || '';
 const OAuth2UserInfoURL = process.env.OAUTH2_USER_INFO_URL || '';
+
+const OAuth2TokenRequestMethod: RequestMethod =
+  (process.env.OAUTH2_TOKEN_REQUEST_METHOD as RequestMethod) || 'get';
+const OAuth2UserInfoMethod: TokenMethod =
+  (process.env.OAUTH2_USER_INFO_REQUEST_METHOD as TokenMethod) || 'get'; // header or get or post
+
 const OAuth2UsernameMap = process.env.OAUTH2_USERNAME_MAP || '';
 const OAuth2AvatarMap = process.env.OAUTH2_AVATAR_MAP || '';
 const OAuth2MemberNameMap = process.env.OAUTH2_MEMBER_NAME_MAP || '';
@@ -21,16 +30,30 @@ export const oauth2_redirectFn: RedirectFn = async ({ redirect_uri }) => {
 export const oauth2_getUserInfo: GetUserInfoFn = async (code: string) => {
   const {
     data: { access_token }
-  } = await axios.get(OAuth2TokenURL, {
+  } = await axios.request({
+    url: OAuth2TokenURL,
+    method: OAuth2TokenRequestMethod,
     params: {
       code
     }
   });
-  const { data } = await axios.get(OAuth2UserInfoURL, {
-    params: {
-      access_token
-    }
+
+  const { data } = await axios.request({
+    url: OAuth2UserInfoURL,
+    method: OAuth2UserInfoMethod,
+    ...(OAuth2UserInfoMethod !== 'header'
+      ? {
+          params: {
+            access_token
+          }
+        }
+      : {
+          headers: {
+            Authorization: `Bearer ${access_token}`
+          }
+        })
   });
+
   const username = data[OAuth2UsernameMap];
   const avatar = data[OAuth2AvatarMap];
   const memberName = data[OAuth2MemberNameMap];
