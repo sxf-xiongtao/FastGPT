4949dd54edf15e6d62bbcb254140e6b1c6656b61
Finley Ge <32237950+FinleyGe@users.noreply.github.com>
2025-04-22 15:23:55 +0800

    perf: sso oauth2 standard (#520)

--- projects/sso/src/provider/oauth2.ts
+++ projects/sso/src/provider/oauth2.ts
@@ -1,27 +1,35 @@
+// OAUTH2.0 Authorization reference: https://www.ruanyifeng.com/blog/2014/05/oauth_2_0.html
 import axios from 'axios';
 import type { GetUserInfoFn, RedirectFn } from 'type';
-
-type RequestMethod = 'get' | 'post';
-type TokenMethod = 'header' | RequestMethod;
-
 const OAuth2AuthorizeURL = process.env.OAUTH2_AUTHORIZE_URL || '';
 const OAuth2TokenURL = process.env.OAUTH2_TOKEN_URL || '';
 const OAuth2UserInfoURL = process.env.OAUTH2_USER_INFO_URL || '';
 
-const OAuth2TokenRequestMethod: RequestMethod =
-  (process.env.OAUTH2_TOKEN_REQUEST_METHOD as RequestMethod) || 'get';
-const OAuth2UserInfoMethod: TokenMethod =
-  (process.env.OAUTH2_USER_INFO_REQUEST_METHOD as TokenMethod) || 'get'; // header or get or post
+const ClientID = process.env.OAUTH2_CLIENT_ID || '';
+const ClientSecret = process.env.OAUTH2_CLIENT_SECRET || '';
+const Scope = process.env.OAUTH2_SCOPE || '';
 
 const OAuth2UsernameMap = process.env.OAUTH2_USERNAME_MAP || '';
 const OAuth2AvatarMap = process.env.OAUTH2_AVATAR_MAP || '';
 const OAuth2MemberNameMap = process.env.OAUTH2_MEMBER_NAME_MAP || '';
 const OAuth2ContactMap = process.env.OAUTH2_CONTACT_MAP || '';
 
-export const oauth2_redirectFn: RedirectFn = async ({ redirect_uri }) => {
+let cache_redirect_uri = '';
+
+export const oauth2_redirectFn: RedirectFn = async ({ redirect_uri, state }) => {
   // parse the redirect_uri
   const url = new URL(OAuth2AuthorizeURL);
   url.searchParams.set('redirect_uri', redirect_uri);
+  url.searchParams.set('client_id', ClientID);
+  url.searchParams.set('response_type', 'code');
+  if (Scope) {
+    url.searchParams.set('scope', Scope);
+  }
+  if (state) {
+    url.searchParams.set('state', state);
+  }
+
+  cache_redirect_uri = redirect_uri;
   return {
     redirectUrl: url.toString()
   };
@@ -32,26 +40,25 @@ export const oauth2_getUserInfo: GetUserInfoFn = async (code: string) => {
     data: { access_token }
   } = await axios.request({
     url: OAuth2TokenURL,
-    method: OAuth2TokenRequestMethod,
-    params: {
-      code
-    }
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: new URLSearchParams({
+      grant_type: 'authorization_code',
+      client_id: ClientID,
+      code,
+      redirect_uri: cache_redirect_uri,
+      ...(ClientSecret ? { client_secret: ClientSecret } : {})
+    })
   });
 
   const { data } = await axios.request({
     url: OAuth2UserInfoURL,
-    method: OAuth2UserInfoMethod,
-    ...(OAuth2UserInfoMethod !== 'header'
-      ? {
-          params: {
-            access_token
-          }
-        }
-      : {
-          headers: {
-            Authorization: `Bearer ${access_token}`
-          }
-        })
+    method: 'get',
+    headers: {
+      Authorization: `Bearer ${access_token}`
+    }
   });
 
   const username = data[OAuth2UsernameMap];
